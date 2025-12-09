2e9b6ba76d92537b57854f725aa29819d6cccb77
aabishek420 <aabishek636@gmail.com>
2025-12-09 23:12:38 +0530

    Home Page Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,26 +5,26 @@ import { MdOutlineFavoriteBorder } from "react-icons/md";
 const Navbar = () => {
   return (
     <>
-      <nav className="flex bg-base-100 sticky top-0 text-base-content justify-between items-center p-4  z-50">
-        <div>
-          <h1 className="text-primary text-2xl font-bold lg:text-4xl">
+      <nav className="flex glass bg-base-100/50 shadow-md sticky top-0 text-base-content justify-between items-center p-4  z-50">
+        <div className="hover:cursor-pointer">
+          <h1 className="text-primary/85 text-2xl font-bold lg:text-4xl hover:text-primary">
             MealBook
           </h1>
-          <p className="text-secondary text-sm font-medium ">
+          <p className="text-secondary/85 text-sm font-medium hover:text-secondary ">
             Every Meal Tells a Story..
           </p>
         </div>
         <div>
-          <ul className="flex gap-4 text-lg font-medium">
-            <div className="flex gap-2 justify-center items-center rounded-full p-2">
+          <ul className="flex gap-4 text-lg font-medium ">
+            <div className="flex gap-2 justify-center items-center rounded-full p-2  hover:cursor-pointer transition group">
               <IoHome className="w-5 h-5 text-primary" />
-              <li className="text-primary hover:font-semibold hover:cursor-pointer transition ">
+              <li className="text-primary group-hover:font-semibold  transition-all duration-200 ">
                 Home
               </li>
             </div>
-            <div className="flex gap-2 justify-center items-center rounded-full p-2">
+            <div className="flex gap-2 justify-center items-center rounded-full p-2 hover:cursor-pointer transition group">
               <MdOutlineFavoriteBorder className="w-5 h-5 text-primary" />
-              <li className="text-primary hover:font-semibold hover:cursor-pointer transition">
+              <li className="text-primary group-hover:font-semibold transition-all duration-200 ">
                 Favorites
               </li>
             </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="min-h-screen flex justify-center items-center">
+        <h2 className='text-xl md:text-2xl lg:text-3xl text-center lg:text-left text-primary font-bold mb-5'>Home Page</h2>
+      </div>
+    </>
+  );
+};
+
+export default Home;
